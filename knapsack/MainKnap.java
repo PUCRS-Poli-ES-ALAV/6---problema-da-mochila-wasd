69ae37ba10aed3d0a1e53bcf22612c2cdcf7b2ae
Caio Ferreira <141863391+caschferr@users.noreply.github.com>
2025-09-08 20:38:27 -0300

    backpackPD
    Não tenho certeza se essas contagens estão certas...

--- knapsack/MainKnap.java
+++ knapsack/MainKnap.java
@@ -14,9 +14,11 @@ public class MainKnap {
         System.out.println("Teste 1 - bruteforcing");
         MyBackpack b1 = new MyBackpack(165);
         ArrayList<MyObj> items = new ArrayList<>();
-
+        
         int[] weights = {23, 31, 29, 44, 53, 38, 63, 85, 89, 82};
         int[] values = {92, 57, 49, 68, 60, 43, 67, 84, 87, 72};
+        int[] weights2 = {56, 59, 80, 64, 75, 17};
+        int[] values2 = {50, 50, 64, 46, 50, 05};
 
         for (int i = 0; i < weights.length; i++) {
             MyObj item = new MyObj(weights[i], values[i]);
@@ -31,8 +33,6 @@ public class MainKnap {
         MyBackpack b2 = new MyBackpack(190);
         items.clear();
 
-        int[] weights2 = {56, 59, 80, 64, 75, 17};
-        int[] values2 = {50, 50, 64, 46, 50, 05};
 
         for (int i = 0; i < weights2.length; i++) {
             MyObj item = new MyObj(weights2[i], values2[i]);
@@ -41,5 +41,36 @@ public class MainKnap {
 
         System.out.printf("%d, iteracoes = %d, instrucoes = %d\n",
         Solves.bruteforce(items, b2, 0), Solves.iterations, Solves.instructions);
+
+
+
+
+
+
+        System.out.println("---------------\nTeste 1 - PD");
+        Solves.resetCounters();
+        items.clear();
+        b1.free();
+
+        for (int i = 0; i < weights.length; i++) {
+            MyObj item = new MyObj(weights[i], values[i]);
+            items.add(item);
+        }
+        System.out.printf("%d, iteracoes = %d, instrucoes = %d\n",
+        Solves.backpackPD(weights.length, b1.getCapacity() , items), Solves.iterations, Solves.instructions);
+
+        System.out.println("---------------\nTeste 2 - PD");
+        Solves.resetCounters();
+        items.clear();
+        b2.free();
+
+        for (int i = 0; i < weights2.length; i++) {
+            MyObj item = new MyObj(weights2[i], values2[i]);
+            items.add(item);
+        }
+
+        System.out.printf("%d, iteracoes = %d, instrucoes = %d\n",
+        Solves.backpackPD(weights2.length, b2.getCapacity(), items), Solves.iterations, Solves.instructions);
     }
+    
 }
--- knapsack/Solves.java
+++ knapsack/Solves.java
@@ -43,4 +43,48 @@ public class Solves {
         instructions ++;
         return maxValue;
     }
+
+    public static int backpackPD (int n, int c, ArrayList<MyObj> itens) {
+        // n = numero de itens
+        // c = capacidade da mochila
+        int[][] maxTab = new int[n+1][c+1];
+        instructions++;
+        for (int i = 0; i < n+1; i++) {
+            iterations++;
+            maxTab[i][0] = 0;
+            instructions++;
+        }
+        for (int j = 0; j < c+1; j++) {
+            iterations++;
+            maxTab[0][j] = 0;
+            instructions++;
+        }
+
+        instructions++;
+        for (int i  = 1; i <= n; i++) {
+            instructions += 2;
+            iterations++;
+
+            instructions++;
+            for (int j = 1; j <= c; j++) {
+                iterations++;
+                instructions += 2;
+
+                instructions += 4;
+                if (itens.get(i-1).getWeight() <= j) {
+                    maxTab[i][j] = Math.max(
+                        maxTab[i-1][j],
+                        itens.get(i-1).getValue() + maxTab[i-1][j - itens.get(i-1).getWeight()]
+                        );
+                    instructions += 13;
+                } else {
+                    maxTab[i][j] = maxTab[i-1][j];
+                    instructions += 3;
+                }
+            }
+        }
+        
+        instructions += 2;
+        return maxTab[n][c];
+    }
 }
