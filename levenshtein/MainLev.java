59c9b24cac9b84fa489ebce53306c7554b8fc07e
Caio Ferreira <141863391+caschferr@users.noreply.github.com>
2025-09-15 20:21:46 -0300

    bruteforce nojento e 7 nao funcional

--- levenshtein/MainLev.java
+++ levenshtein/MainLev.java
@@ -1,5 +1,51 @@
 package levenshtein;
 
 public class MainLev {
-    
+    public static void main(String[] args) {
+        String from1 = "Casablanca";
+        String to1   = "Portentoso";
+
+        String from2 = "Maven, a Yiddish word meaning accumulator of knowledge, began as an attempt to " +
+            "simplify the build processes in the Jakarta Turbine project. There were several" + 
+   			" projects, each with their own Ant build files, that were all slightly different." +
+   			"JARs were checked into CVS. We wanted a standard way to build the projects, a clear "+ 
+   			"definition of what the project consisted of, an easy way to publish project information" +
+   			"and a way to share JARs across several projects. The result is a tool that can now be" +
+   			"used for building and managing any Java-based project. We hope that we have created " +
+   			"something that will make the day-to-day work of Java developers easier and generally help " +
+   			"with the comprehension of any Java-based project.";
+        String to2 = "This post is not about deep learning. But it could be might as well. This is the power of " +
+   			"kernels. They are universally applicable in any machine learning algorithm. Why you might" +
+   			"ask? I am going to try to answer this question in this article." + 
+   		    "Go to the profile of Marin Vlastelica Pogančić" + 
+   		    "Marin Vlastelica Pogančić Jun";
+
+        Solves.resetCounters();
+        long start = System.currentTimeMillis();
+        int distance1 = Solves.bruteforce(from1, to1);
+        long end = System.currentTimeMillis();
+        System.out.println("-------- bruteforce 1 --------");
+        System.out.printf("distance = %d, iterations = %d, instructions = %d, time = %dms\n",
+                          distance1, Solves.iterations, Solves.instructions, end - start);
+
+        Solves.resetCounters();
+
+        // trust me you do not want to run this lmao
+
+        // start = System.currentTimeMillis();
+        // int distance2 = Solves.bruteforce(from2, to2);
+        // end = System.currentTimeMillis();
+        // System.out.println("-------- bruteforce 2 --------");
+        // System.out.printf("distance = %d, iterations = %d, instructions = %d, time = %dms\n",
+        //                   distance2, Solves.iterations, Solves.instructions, end - start);
+
+        Solves.resetCounters();
+
+        start = System.currentTimeMillis();
+        int distance3 = Solves.distEdProgDina(from1, to1);
+        end = System.currentTimeMillis();
+        System.out.println("-------- distEdProgDina 1 --------");
+        System.out.printf("distance = %d, iterations = %d, instructions = %d, time = %dms\n",
+                          distance3, Solves.iterations, Solves.instructions, end - start);
+    }
 }
--- levenshtein/Solves.java
+++ levenshtein/Solves.java
@@ -1,15 +1,88 @@
 package levenshtein;
 
 public class Solves {
-    public static int iterations = 0;
-    public static int instructions = 0;
+    public static long iterations = 0;
+    public static long instructions = 0;
     
     public static void resetCounters() {
         iterations = 0;
         instructions = 0;
     }
 
-    public static void bruteforce() {
+    public static int bruteforce(String from, String to) {
+        // i = [0..m), where m = from.length()
+        // j = [0..n), where n = to.length()
+
+        return bruteforceRec(from, to, 0, 0);
+    }
+
+    private static int bruteforceRec(String from, String to, int i, int j) {
+        iterations++;
+
+        // base cases
+        if (i >= from.length() && j >= to.length()) {
+            instructions += 6;
+            return 0;
+        }
+        else if (i >= from.length()) {
+            instructions += 10;
+            return to.length() - j;
+        }
+        else if (j >= to.length()) {
+            instructions += 12;
+            return from.length() - i;
+        }
         
+        // recursive cases
+
+        // match
+        if (from.charAt(i) == to.charAt(j)) {
+            instructions += 7;
+            return bruteforceRec(from, to, i + 1, j + 1);
+        }
+
+        instructions += 3;
+        
+        // substitution
+        int substitution = 1 + bruteforceRec(from, to, i + 1, j + 1);
+        // insertion
+        int insertion = 1 + bruteforceRec(from, to, i, j + 1);
+        // deletion
+        int deletion = 1 + bruteforceRec(from, to, i + 1, j);
+
+        instructions += 13;
+        
+        // return minimum of the three
+        instructions += 3;
+        return Math.min(substitution, Math.min(insertion, deletion));
+    }
+
+    public static int distEdProgDina(String from, String to) {
+        int m = from.length();
+        int n = to.length();
+
+        int[][] table = new int[m + 1][n + 1];
+        table[0][0] = 0;
+
+        for (int i = 1; i < m; i++) table[i][0] = table[i - 1][0] + 1;
+        for (int j = 1; j < n; j++) table[0][j] = table[0][j - 1] + 1;
+
+        int extraCost;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (from.charAt(i) == to.charAt(j)) {
+                    extraCost = 0; // match
+                } else {
+                    extraCost = 1; // substitution
+                }
+
+                table[i][j] = Math.min(table[i-1][j] + 1,
+                                   Math.min(table[i][j-1] + 1,
+                                            table[i-1][j-1] + extraCost));
+            }
+        }
+
+        return table[m-1][n-1];
     }
 }
